45b5bce6e32deb08766a73daf0a1ad952b78e3ed
JoseIgnacioTapia <josignaciot@gmail.com>
2023-09-04 10:45:01 -0300

    Routing of my application

--- models/server.ts
+++ models/server.ts
@@ -1,12 +1,24 @@
 import express, { Application } from "express";
 
+import useRoutes from "../routes/usuario";
+
 class Server {
   private app: Application;
   private port: string;
+  private apiPaths = {
+    usuarios: "/api/usuarios",
+  };
 
   constructor() {
     this.app = express();
     this.port = process.env.PORT || "8000";
+
+    // Definir mis rutas
+    this.routes();
+  }
+
+  routes() {
+    this.app.use(this.apiPaths.usuarios, useRoutes);
   }
 
   listen() {
