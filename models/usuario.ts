fb410b9563f0d001e34d0b63bce0009f91fe91d5
JoseIgnacioTapia <josignaciot@gmail.com>
2023-09-05 16:40:31 -0300

    Usuario model

---
+++ models/usuario.ts
@@ -0,0 +1,21 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../db/connection";
+
+const Usuario = sequelize.define("Usuario", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  nombre: {
+    type: DataTypes.STRING,
+  },
+  email: {
+    type: DataTypes.STRING,
+  },
+  estado: {
+    type: DataTypes.BOOLEAN,
+  },
+});
+
+export default Usuario;
